3c04a78dc75cd84b60c645414cf795c97ebb70e4
Sami Jaffri <114799563+Sami-Jaffri@users.noreply.github.com>
2025-02-06 16:17:11 -0800

    Designing and Login Page

--- student-records-app/src/App.js
+++ student-records-app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses"; // Import Courses page
+import CreateUser from "./createUser";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/courses" element={<Courses />} /> {/* Courses Route */}
+        <Route path="/createUser" element={<CreateUser />} />
       </Routes>
     </Router>
   );
