c79288c12ac9b3c3b0c338431899994d6e9dd88c
evan-pasenau <evan.pas@icloud.com>
2025-02-06 22:35:21 -0800

    payment page created

--- student-records-app/src/App.js
+++ student-records-app/src/App.js
@@ -3,6 +3,7 @@ import Login from "./Login";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses"; // Import Courses page
 import Finances from "./Finances";
+import MakePayment from "./MakePayment";
 import CreateUser from "./createUser";
 
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/courses" element={<Courses />} /> {/* Courses Route */}
         <Route path="/finances" element={<Finances />} />
+        <Route path="/makePayment" element={<MakePayment />} />
       </Routes>
     </Router>
   );
