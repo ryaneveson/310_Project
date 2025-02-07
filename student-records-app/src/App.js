e58893b6253209b0bc14969b6e24f7b17701a92a
JaxHay <jaxonmhay@gmail.com>
2025-02-07 12:10:06 -0800

    editGrades UI, header footer implemented. Still need to connect to database

--- student-records-app/src/App.js
+++ student-records-app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses"; // Import Courses page
+import EditGrades from './editGrades';
 import CreateUser from "./createUser";
 
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/courses" element={<Courses />} /> {/* Courses Route */}
+        <Route path="/editGrades" element={<EditGrades />} />
       </Routes>
     </Router>
   );
