e9f4c410d13ecdd58bb9fa6c351a7802d4168bdc
Abdalla Haider Abbas <abendos203@gmail.com>
2025-03-06 15:10:15 -0800

    tweaked

--- student-records-app/src/Courses.js
+++ student-records-app/src/Courses.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation} from "react-router-dom";
 import "./frontend/courses.css";
 
 function Courses() {
